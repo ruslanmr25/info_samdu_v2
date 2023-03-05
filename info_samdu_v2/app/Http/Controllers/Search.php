891719daf027ac4e3899bf8da38817a5f92c6370
ruslanmr25 <dilshodahmedovvv@gmail.com>
2023-03-05 21:49:47 +0500

    'asdf'

--- info_samdu_v2/app/Http/Controllers/Search.php
+++ info_samdu_v2/app/Http/Controllers/Search.php
@@ -4,7 +4,9 @@ namespace App\Http\Controllers;
 
 use App\Models\EducationalInformation;
 use App\Models\Student;
+use Illuminate\Contracts\Database\Eloquent\Builder;
 use Illuminate\Http\Request;
+use PhpParser\Node\Stmt\Return_;
 use PHPUnit\Framework\MockObject\Builder\Stub;
 
 class Search extends Controller
@@ -18,48 +20,73 @@ class Search extends Controller
     public function __invoke(Request $request)
     {
 
-        // $search_educ=$request->eductional_information;
-        $sear_student=$request->student;
+        $query = Student::query();
 
-        return $request->all();
+        if ($request->students) {
 
+            foreach ($request->students as $key => $value) {
 
-
-        
-        $query=EducationalInformation::query();
-
-
-        $condition=[];
-        if($request->department){
-             $condition['department_id']=$request->department;
-        }
-
-        if($request->specialty){
-            $condition['specialty']=$request->specialty;
-        }
-
-        if($request->level){
-            $condition['level']=$request->level;
-        }
-
-        if($request->paymentForm){
-            $condition['paymentForm']=$request->paymentForm;
-        }
-        if($request->group){
-            $condition['group']=$request->group;
+                $query->where($key, 'LIKE', '%' . $value . '%');
+            }
         }
 
 
 
-        foreach($condition as $key=>$value){
-            $query->where($key,'LIKE','%'.$value.'%');
+
+        if ($request->educational_information) {
+            $query->whereRelation('educational_information', function ($educ_query) use ($request) {
+
+
+                return $educ_query->where($request->educational_information);
+            });
 
 
 
         }
-        $students=$query->get();
+        return $query->with('educational_information')->get();
 
-        return $students;
+        // // $search_educ=$request->eductional_information;
+        // $sear_student=$request->student;
+
+        // return $request->all();
+
+
+
+
+        // $query=EducationalInformation::query();
+
+
+        // $condition=[];
+        // if($request->department){
+        //      $condition['department_id']=$request->department;
+        // }
+
+        // if($request->specialty){
+        //     $condition['specialty']=$request->specialty;
+        // }
+
+        // if($request->level){
+        //     $condition['level']=$request->level;
+        // }
+
+        // if($request->paymentForm){
+        //     $condition['paymentForm']=$request->paymentForm;
+        // }
+        // if($request->group){
+        //     $condition['group']=$request->group;
+        // }
+
+
+
+        // foreach($condition as $key=>$value){
+        //     $query->where($key,'LIKE','%'.$value.'%');
+
+
+
+        // }
+        // $students=$query->get();
+
+        // return $students;
 
 
 
